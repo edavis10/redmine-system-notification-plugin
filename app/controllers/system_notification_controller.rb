aa0168e8fa99e24ed83830e24ea6986b04eeb0b0
Eric Davis <edavis@littlestreamsoftware.com>
2008-12-02 12:40:55 -0800

    Building up SystemNotificationController#create to send out the notification

--- app/controllers/system_notification_controller.rb
+++ app/controllers/system_notification_controller.rb
@@ -6,4 +6,11 @@ class SystemNotificationController < ApplicationController
   def index
     @system_notification = SystemNotification.new
   end
+  
+  def create
+    @system_notification = SystemNotification.new(params[:system_notification])
+    @system_notification.users = SystemNotification.users_since(params[:system_notification][:time]) if params[:system_notification][:time]
+    flash[:notice] = "System Notification was successfully sent."
+    redirect_to :action => 'index'
+  end
 end
--- spec/controllers/system_notification_controller_spec.rb
+++ spec/controllers/system_notification_controller_spec.rb
@@ -20,3 +20,42 @@ describe SystemNotificationController do
     response.should_not be_success
   end
 end
+
+describe SystemNotificationController,'#create with valid SystemNotification' do
+  before(:each) do
+    admin = mock_model(User, :admin? => true, :logged? => true)
+    User.stub!(:current).at_least(:once).and_return(admin)
+  end
+  
+
+  it 'should redirect to the #index' do
+    SystemNotification.stub!(:users_since)
+    post :create, :system_notification => { :subject => 'Test', :body => 'A notification', :time => 'week'}
+    response.should be_redirect
+    response.should redirect_to(:controller => 'system_notification', :action => 'index')
+  end
+  
+  it 'should display a message to the user' do
+    SystemNotification.stub!(:users_since)
+    post :create, :system_notification => { :subject => 'Test', :body => 'A notification', :time => 'week'}
+    flash[:notice].should match(/success/)
+  end
+  
+  it 'should assign @system_notification for the view' do
+    SystemNotification.should_receive(:users_since).with('week').and_return([])
+    post :create, :system_notification => { :subject => 'Test', :body => 'A notification', :time => 'week'}
+    assigns[:system_notification].should_not be_nil
+  end
+  
+  it 'should get the users based on the Time' do
+    user1 = mock_model(User)
+    user2 = mock_model(User)
+    users = [user1, user2]
+    SystemNotification.should_receive(:users_since).with('week').and_return(users)
+    post :create, :system_notification => { :subject => 'Test', :body => 'A notification', :time => 'week'}
+    assigns[:system_notification].users.should eql(users)
+  end
+
+  it 'should deliver the SystemNotification'
+
+end
