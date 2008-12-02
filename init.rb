cf0bb332806ce78dcf906a8bb53581a547466369
Eric Davis <edavis@littlestreamsoftware.com>
2008-12-02 10:35:52 -0800

    Adding generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :system_notification do
+  name 'Redmine System Notification plugin'
+  author 'Eric Davis'
+  description 'This is a plugin for Redmine to allow an Administrator to send systemwide notifications to specific users'
+  version '0.1.0'
+end
