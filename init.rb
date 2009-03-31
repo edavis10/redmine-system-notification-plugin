5271b6497605478893b1bc8800676cfe4e6f8019
Eric Davis <edavis@littlestreamsoftware.com>
2009-03-31 12:57:11 -0700

    Bumping version to 0.2.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :system_notification_plugin do
   name 'Redmine System Notification plugin'
   author 'Eric Davis'
   description 'This is a plugin for Redmine to allow an Administrator to send systemwide notifications to specific users'
-  version '0.1.0'
+  version '0.2.0'
   
   menu :admin_menu, :system_notification, { :controller => 'system_notification', :action => 'index'}, :caption => :system_notification
 end
